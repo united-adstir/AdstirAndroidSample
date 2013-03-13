4ba010329437ba01ec51c74d5b075a8179502257
reiji-terasaka <reiji-terasaka@united.jp>
2013-03-13 17:27:21 +0900

    License

--- src/com/ad_stir/sample/AdStirSampleActivity.java
+++ src/com/ad_stir/sample/AdStirSampleActivity.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012 motionBEAT Inc.
+   Copyright 2012-2013 UNITED, inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
